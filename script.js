5ad6caac08befe8259f2a8faa7497d20d27f2a9e
Seferino Garcia <garciaseferino26@gmail.com>
2023-07-08 02:38:55 -0500

    all buttons are writting onto the display area resetting display to blank if operatior is detected

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // select the calculator body and append buttons for later use
 const buttons = document.querySelector(".buttons");
 const symbols = ["7", "8", "9", "÷", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"]
+const numbers = ["7", "8", "9", ".", "4", "5", "6", "1", "2", "3", "0"]
+const operators = ["x", "÷", "-", "+"]
 let count = 0;
 
 for (let i=0; i<4; i++) {
@@ -21,9 +23,61 @@ for (let i=0; i<4; i++) {
 }
 
 
-const eachButton = document.querySelectorAll("button").forEach(item => {
+const eachButton = document.querySelectorAll(".buttonItem").forEach(item => {
     item.addEventListener('click', function() {
-        console.log(item.textContent)
+        const typing = document.querySelector(".prints");
+
+        //typing.textContent += item.textContent;
+        if (operators.includes(typing.textContent[0])) {
+            typing.textContent = ""
+        }
+
+        if (numbers.includes(item.textContent)) {
+            typing.textContent += item.textContent;
+        };
+
+        if (operators.includes(item.textContent)) {
+            typing.textContent = "" 
+            typing.textContent += item.textContent;
+        }
     })
 })
 
+
+// function for each operator
+const add = function(x, y) {
+	return parseFloat(x) + parseFloat(y);
+};
+
+const subtract = function(x, y) {
+	return parseFloat(x) - parseFloat(y);
+};
+
+const multiply = function(x, y) {
+    return parseFloat(x) * parseFloat(y);
+};
+
+const divide = function(x, y) {
+    return (parseFloat(x) / parseFloat(y)).toFixed(2);
+}
+
+// operator function
+const operator = function(num1, oper, num2) {
+    if (oper === "+") {
+        return add(num1, num2);
+    } else if (oper === "-") {
+        return subtract(num1, num2);
+    } else if (oper === "x") {
+        return subtract(num1, num2);
+    } else if (oper === "÷") {
+        return divide(num1, num2);
+    }  
+}
+
+
+console.log(add('5',7))
+console.log(subtract(5,7))
+console.log(multiply(5,7))
+console.log(divide(14,5))
+
+console.log(operator(10, '+', 5))
